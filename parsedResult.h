701996177bc642745fe43fc86e92e67c25d3bfda
Erick Santinon Gomes <erick.dicksan@gmail.com>
2024-05-28 14:22:40 -0300

    refactor: create header to ParsedResult.

---
+++ parsedResult.h
@@ -0,0 +1,17 @@
+//
+// Created by Erick Gomes on 28/05/24.
+//
+
+#ifndef PARSEDRESULT_H
+#define PARSEDRESULT_H
+
+typedef struct {
+    char firstChar;
+    char remainingStr[255];
+} ParsedResult;
+
+ParsedResult parse(const char *input);
+char getChar(ParsedResult parsed_result);
+char * getName(ParsedResult parsed_result);
+
+#endif //PARSEDRESULT_H
