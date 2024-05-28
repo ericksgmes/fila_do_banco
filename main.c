a1e38eafd4453b9eb35bf26180a545c5b9660e43
Erick Santinon Gomes <erick.dicksan@gmail.com>
2024-05-28 16:00:13 -0300

    feat: create fila_do_banco logic.

--- main.c
+++ main.c
@@ -14,15 +14,31 @@ int main() {
     t_queue fila_geral, fila_prioridade;
     create(&fila_geral, 20);
     create(&fila_prioridade, 20);
+    int counter = 0;
 
     while (1) {
         scanf(" %[^\n]", &n);
         parsed_result = parse(n);
+        ParsedResult outVal;
         if (getChar(parsed_result) == 'f') {
             break;
         }
         if (getChar(parsed_result) == 'g') {
-            in(fila_geral);
+            in(&fila_geral, parsed_result);
+        }
+        if (getChar(parsed_result) == 'p') {
+            in(&fila_prioridade, parsed_result);
+        }
+        if (getChar(parsed_result) == 's') {
+            if (is_empty(&fila_prioridade) || counter == 3) {
+                out(&fila_geral, &outVal);
+                printf("%s\n", getName(outVal));
+                counter = 0;
+            } else {
+                out(&fila_prioridade, &outVal);
+                printf("%s\n", getName(outVal));
+                counter++;
+            }
         }
     }
 
